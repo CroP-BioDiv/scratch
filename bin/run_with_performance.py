05faf1489a021faace0d94523f28532e85c31203
Ante Turudic <ante.turudic@gmail.com>
2018-08-26 13:07:17 +0200

    Performance script update

--- bin/run_with_performance.py
+++ bin/run_with_performance.py
@@ -29,6 +29,7 @@ class RunWithPerformance:
                  performance=True,
                  perf_seconds=60,
                  **kwargs):  # Other not used parameters
+
         assert command
         assert type(command) is str, command
 
@@ -38,50 +39,63 @@ class RunWithPerformance:
         else:
             self.output_directory = os.getcwd()
         self.time_it = time_it
-        # self.std = std
         self.performance = performance
         self.perf_seconds = perf_seconds
         #
         o = self.output_directory
         self._times = os.path.join(o, '_times.out')
         self._performance = os.path.join(o, '_performance.out')
-        if self.std:
+        if std:
             self._stdout = os.path.join(o, '_stdout.out')
             self._stderr = os.path.join(o, '_stderr.out')
         else:
             self._stdout = None
             self._stderr = None
+        #
+        self._main_c = os.path.basename(command.split()[0])
 
     def _run_command(self):
-        subprocess.call(self.command, stdout=self._stdout, stderr=self._stderr)
+        if self._stdout:
+            subprocess.call(self.command, shell=True,
+                            stdout=open(self._stdout, 'w'),
+                            stderr=open(self._stderr, 'w'))
+        else:
+            subprocess.call(self.command, shell=True)
 
     def _run_pidstat(self):
-        # pid = os.getpid()
-        pid = self._p_run.pid
-        cmd = 'pidstat {} -rud -p {}'.format(self.perf_seconds, pid)
-        subprocess.call(cmd, stdout=self._performance)
+        cmd = 'pidstat {} -rud -C {}'.format(self.perf_seconds, self._main_c)
+        print(cmd)
+        subprocess.call(cmd, shell=True, stdout=open(self._performance, 'w'))
 
     def run(self):
         if self.time_it:
+            print('INFO: Writing start time!')
             with open(self._times, 'w') as f:
-                f.write("started: %s\n".format(
+                f.write("started: {}\n".format(
                     datetime.datetime.now().isoformat()))
 
+        print('INFO: Starting command!')
         self._p_run = Process(target=self._run_command)
-        self._p_run.run()
+        self._p_run.start()
+        print('INFO: Command started!')
 
         if self.performance:
+            print('INFO: Starting pidstat!')
             self._p_pidstat = Process(target=self._run_pidstat)
-            self._p_pidstat.run()
+            self._p_pidstat.start()
+            print('INFO: pidstat started!')
 
+        print('INFO: Joining!')
         self._p_run.join()
+        print('INFO: Finished!')
 
         if self.performance:
             self._p_pidstat.terminate()
 
         if self.time_it:
+            print('INFO: Writing end time!')
             with open(self._times, 'a') as f:
-                f.write("ended: %s\n".format(
+                f.write("ended: {}\n".format(
                     datetime.datetime.now().isoformat()))
 
 
